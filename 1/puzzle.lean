d0e97313cbee49e460ce187d5f99e0df29e2c26c
Omri Schwarz <omri.schwarz@ARL-DYCPQTJJHN.home>
2024-12-01 14:01:55 -0500

    progress

--- 1/puzzle.lean
+++ 1/puzzle.lean
@@ -1,3 +1,21 @@
+--import Init.Data.List.Basic
+def sum (xs : List Nat) : Nat :=
+  xs.foldl (init := 0) (·+·)
 
-def match1 (x : [Nat]) ( y: Nat) : Nat := 
-  y * sum 
+
+
+declare_syntax_cat compClause
+syntax "for " term " in " term : compClause
+syntax "if " term : compClause
+
+syntax "[" term " | " compClause,* "]" : term
+def List.map' (xs : List α) (f : α → β) : List β := List.map f xs
+
+macro_rules
+  | `([$t:term |]) => `([$t])
+  | `([$t:term | for $x in $xs]) => `(List.map' $xs  (λ $x => $t))
+  | `([$t:term | if $x]) => `(if $x then [$t] else [])
+  | `([$t:term | $c, $cs,*]) => `(List.join [[$t | $cs,*] | $c])
+
+def match1 (xs : List Nat ) ( y: Nat) : Nat := 
+  sum [ y | for a in xs, if a = y]
