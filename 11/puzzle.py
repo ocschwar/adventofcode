3e27ff18418d16651ed91ff7ef0cd2d30fc9bb89
Omri Schwarz <omri.schwarz@ARL-DYCPQTJJHN.local>
2024-12-11 00:43:59 -0500

    stage 2 works

--- 11/puzzle.py
+++ 11/puzzle.py
@@ -24,13 +24,47 @@ def blink(L):
                 LC[i+ii] =l* 2024
     return LC
 
+def sub_blink(D):
+    nd = {}
+    for l,v in D.items():
+        if not v:
+            continue
+        if l == 0 :
+            nl = [1] 
+        else:
+            s = str(l)
+            if len(s)%2==0:
+                a = s[:len(s)//2]
+                b = s[len(s)//2:]
+                nl = [int(a),int(b)]
+            else:
+                nl =[l* 2024]
+        for n in nl:
+            nd[n] = nd.get(n,0) + v
+    return nd 
+        
+    
+
+def blink_2 (L):
+    D = {}
+    for l in L:
+        D[l] =D.get(l,0) + 1
+    return D
+
 def main(args):
     LL = list(map(int,open(args[1]).readlines()[0].rstrip().split(' ')))
-    print(LL)
+    #print(LL)
+    #for i in range(int(args[2])):
+    #    LL = blink(LL)
+    #    print('LL',LL)    
+    #    print('LL',len(LL))
+    D = blink_2(LL)
+    print(D, len(D))
     for i in range(int(args[2])):
-        LL = blink(LL)
-        print('LL',LL)
-        print(len(LL))
+        D = sub_blink(D)
+        S = sum(D.values())
+        print(i+1, D, S)
+        
         
 if __name__ == "__main__":
     main(sys.argv)
