62635e7fbe07701d6f8b52ca13d1a8709207953c
Omri Schwarz <omri.schwarz@ARL-DYCPQTJJHN.home>
2024-12-14 17:10:09 -0500

    works.,

--- 12/puzzle.py
+++ 12/puzzle.py
@@ -27,43 +27,32 @@ def count_fences(LL, P, I, J, FF):
     FF[P[0]][P[1]] = ret
     return ret
 def count_sides(LL, S,I,J, RR):    
-    start = s = sorted(S)[0]
-    c =LL[s[0]][s[1]]
-    sides = 1
-    d = (-1,0)
-    nbrs = set()
-    while True: 
-        r = rots[d]
-        N = (s[0]+d[0], s[1]+d[1])
-        if ( N[0] >= 0 and N[1] >= 0 and
-             N[0] <J and N[1] <J):
-            nbrs.add(RR[N[0]][N[1]])
-        else:
-            nbrs.add(-1)
-        P = (s[0]+r[0], s[1]+r[1])
-        print(c, s, 'd', d, 'r', r,'P', P, P in S, sides)
-        if P in S:
-            P_ = (P[0]+d[0], P[1]+d[1])
-            print(c,'2',s, d, r,P, P_, P_ in S)
-            if P_ in S:
-                # left turn!
-                sides += 1
-                s = P_
-                d = rots[rots[r]]#(-d[0],-d[1])
-                print("LEFT", P_, d )
-                if s == start:
-                    sides+=1
-            else:
-                s = P
-        else:
-            print("RT")
-            # right turn.
-            sides += 1
-            d = r
-        if sides>3 and s == start:
-            break
-    return (sides-sides%2,nbrs)
-            
+    corners = 0
+    for s in S:
+        up = (s[0] -1 ,s[1]) 
+        lt = (s[0] ,s[1]-1 )
+        dn = (s[0] +1 ,s[1]) 
+        rt = (s[0] ,s[1]+1 )
+        if (up not in S and lt not in S):# and (s[0]-1,s[1]-1) not in S):
+            corners += 1
+        if (dn not in S and rt not in S):# and (s[0]-1,s[1]-1) not in S):
+            corners += 1
+        if (up not in S and rt not in S):# and (s[0]-1,s[1]-1) not in S):
+            corners += 1
+        if (dn not in S and lt not in S):# and (s[0]-1,s[1]-1) not in S):
+            corners += 1
+        #if (dn not in S and rt not in S):            
+        #    corners += 2
+        #else:
+        if (up in S and lt in S and (s[0]-1,s[1]-1) not in S):
+            corners += 1
+        if (dn in S and rt in S and (s[0]+1,s[1]+1) not in S):
+            corners += 1
+        if (up in S and rt in S and (s[0]-1,s[1]+1) not in S):
+            corners += 1
+        if (dn in S and lt in S and (s[0]+1,s[1]-1) not in S):
+            corners += 1
+    return corners
     # top left square, so it's facing "north".
     # and the side runs east...
     
@@ -120,23 +109,11 @@ def main(args):
                 D[ret[0]] = {
                     'size':ret[1],'pts':ret[2]}
     for k,v in D.items():
-        sides,nbrs = count_sides(LL, v['pts'], I, J,RR)
+        sides = count_sides(LL, v['pts'], I, J,RR)
         v['sides']=sides
-        v['nbrs'] = nbrs
+    pprint.pprint(D)
     price = sum([v['size']*v['sides'] for v in D.values()])
     print('price',price)
-    pprint.pprint(("D",D))
-    for k,v in D.items():
-        if len(v['nbrs'])==1 :
-            N = list(v['nbrs'])[0]
-            if N == -1 :
-                continue
-            if k in D[N]['nbrs']:
-                print('SKIP',k, v,D[N]['size'])
-                continue
-            price+= v['sides'] * D[N]['size']
-    print('price',price)
-    return
 
 def foo():
     D = {}
