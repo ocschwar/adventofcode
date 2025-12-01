5bebbd18d5f360c99e64d87ea3d91ee99f6e732e
Omri.Schwarz <omri.schwarz@FE-D0HPFWQ1F4.local>
2025-12-01 17:39:58 -0500

    verbosity to help Verus impl

--- 1/puzzle.py
+++ 1/puzzle.py
@@ -16,21 +16,25 @@ def main_1(args):
 def main(args):
     lines = open(args[1]).readlines()
     start = 50
-    hits = 0 
+    hits = 0
+    twits = [0,0,0]
     dirs = {"R":1, "L":-1}
     for l in lines:
         move = int(l[1:])
         sign = dirs[l[0]]
         hits += move//100
+        twits[2] += move//100
         mod = move % 100
         if start > 0 : 
             if sign == 1 and start + mod >= 100 :
+                twits[0] += 1 
                 hits+=1
             elif sign == -1 and start - mod <= 0 :
+                twits[1] += 1 
                 hits += 1
         start += sign*move
         start %= 100 
-    print(start, hits)
+    print(start, hits, twits)
         
 
 
