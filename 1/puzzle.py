115ee7ab58750e887bb446482d7a64888bafceb3
Omri.Schwarz <omri.schwarz@FE-D0HPFWQ1F4.local>
2025-12-01 00:52:19 -0500

    first star done. second is stumping me.

---
+++ 1/puzzle.py
@@ -0,0 +1,37 @@
+import sys
+import math
+
+
+def main_1(args):
+    lines = open(args[1]).readlines()
+    start = 50
+    hits = 0 
+    dirs = {"R":1, "L":-1}
+    for l in lines:
+        start += dirs[l[0]] * int(l[1:])
+        start %= 100
+        if start == 0 :
+            hits +=1 
+    print(start, hits)
+def main(args):
+    lines = open(args[1]).readlines()
+    start = 50
+    hits = 0 
+    dirs = {"R":1, "L":-1}
+    for l in lines:
+        move = int(l[1:])
+        sign = dirs[l[0]]
+        hits += move//100
+        mod = move % 100
+        if sign == 1 and start + mod >= 100 :
+            hits+=1
+        elif sign == -1 and start - mod <= 0 :
+            hits += 1
+        start += sign*move
+        start %= 100 
+    print(start, hits)
+        
+
+
+if __name__=="__main__":
+    main(sys.argv)
